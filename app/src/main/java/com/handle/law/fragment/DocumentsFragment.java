6721fe0514d78978861f7c2251b6cecd9ad99ebf
WrapOnly <xiyang1321tt@126.com>
2016-04-28 14:35:23 +0800

    new document view.

--- app/src/main/java/com/handle/law/fragment/DocumentsFragment.java
+++ app/src/main/java/com/handle/law/fragment/DocumentsFragment.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import com.handle.law.R;
 import com.handle.law.base.BaseFragment;
@@ -12,12 +15,16 @@ import com.handle.law.base.BaseFragment;
 /**
  * Created by Administrator on 2016/4/25.
  */
-public class DocumentsFragment extends BaseFragment implements View.OnClickListener{
+public class DocumentsFragment extends BaseFragment implements View.OnClickListener, AdapterView.OnItemClickListener{
+
+    private GridView gvDocuments;
+    private DocumentsAdapter adapter;
+    private String[] datas = {"报账单", "付款单", "入库单", "出库单", "收款单"};
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.mine_fragment_layout, container, false);
+        View view = inflater.inflate(R.layout.document_fragment_layout, container, false);
         this.initViews(view);
         this.setListener();
         return view;
@@ -25,16 +32,23 @@ public class DocumentsFragment extends BaseFragment implements View.OnClickListe
 
     @Override
     protected void initViews(View _view) {
-
+        this.gvDocuments = (GridView) _view.findViewById(R.id.gv_document_fragment);
+        this.adapter = new DocumentsAdapter(getActivity(), datas);
+        this.gvDocuments.setAdapter(adapter);
     }
 
     @Override
     protected void setListener() {
-
+        this.gvDocuments.setOnItemClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "点击的位置"+position, Toast.LENGTH_SHORT).show();
     }
 }
